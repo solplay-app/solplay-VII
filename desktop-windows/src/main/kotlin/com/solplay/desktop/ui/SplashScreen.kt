746df8a10cb4f95cd7f32d2558087405304c247f
solplay <287523053+solplay-app@users.noreply.github.com>
2026-07-18 12:04:12 +0100

    Update SplashScreen.kt

--- desktop-windows/src/main/kotlin/com/solplay/desktop/ui/SplashScreen.kt
+++ desktop-windows/src/main/kotlin/com/solplay/desktop/ui/SplashScreen.kt
@@ -1,5 +1,6 @@
 package com.solplay.desktop.ui
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
@@ -7,6 +8,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.delay
 
